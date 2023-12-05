a2f9abe49bebb4621b7e1cc9b410e69010171cc9
F <sfr007@ya.ru>
2023-12-05 13:41:54 +0300

    added handling SettingsContainerFragment onBackPressed

--- app/src/main/java/sitec_it/ru/androidapp/ui/settings/SettingsContainerFragment.kt
+++ app/src/main/java/sitec_it/ru/androidapp/ui/settings/SettingsContainerFragment.kt
@@ -3,11 +3,22 @@ package sitec_it.ru.androidapp.ui.settings
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import sitec_it.ru.androidapp.R
+import sitec_it.ru.androidapp.ui.MainFragment
 
 class SettingsContainerFragment: Fragment(R.layout.fragment_settings_container) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This callback is only called when MyFragment is at least started
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.nav_host_fragment, MainFragment())?.commit()
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -30,5 +41,7 @@ class SettingsContainerFragment: Fragment(R.layout.fragment_settings_container) 
 
         if (savedInstanceState == null) activity?.supportFragmentManager?.beginTransaction()
             ?.replace(R.id.settings_nav_host_fragment, BaseSettingsFragment())?.commit()
+
+
     }
 }
